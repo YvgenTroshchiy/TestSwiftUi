4aaa121586e744b058dd5e2b52c15e12580f7143
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2024-01-17 12:39:53 +0200

    Initial Commit

---
+++ TestSwiftUi/TestSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUiApp.swift
+//  TestSwiftUi
+//
+//  Created by Yvgen Troshchiy on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

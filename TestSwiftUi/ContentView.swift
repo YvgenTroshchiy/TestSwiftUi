e039a69f9aa7cc37b0ef9c242ec8196df99eac70
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2024-02-02 17:46:48 +0200

    Add logger

--- TestSwiftUi/ContentView.swift
+++ TestSwiftUi/ContentView.swift
@@ -4,6 +4,7 @@ import UIKit
 struct ContentView: View {
     var body: some View {
         SuccessIndicatorView {
+            MyLogger.logger.debug("Logger onViewTap")
             print("onViewTap")
         }
     }
--- TestSwiftUi/TestSwiftUiApp.swift
+++ TestSwiftUi/TestSwiftUiApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
+import OSLog
 
 @main
 struct TestSwiftUiApp: App {
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+struct MyLogger {
+    static let logger = Logger(subsystem: "com.troshchiy", category: "test app")
 }
